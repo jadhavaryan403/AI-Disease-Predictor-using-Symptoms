d553c3784d3fa719dbbb07495273e4b013dc2719
Aryan Jadhav 403 <jadhavaryan403@gmail.com>
2026-01-09 18:39:26 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'backend.prediction'
+    'prediction'
 ]
 
 MIDDLEWARE = [
