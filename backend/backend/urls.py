44c4f53903ecb1c1e74bcb3c151da34482d3d314
Aryan Jadhav 403 <jadhavaryan403@gmail.com>
2026-01-09 19:07:33 +0530

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from backend.prediction import views
+from prediction import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
